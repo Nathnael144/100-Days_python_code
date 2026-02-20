2e153b8278abc352dcff5cee67eef7a974bbfb35
nathnael144 <nathnael.biresaw@gmail.com>
2026-02-20 11:30:29 +0500

    Add Day2.py with examples of data types and basic operations

---
+++ Day2.py
@@ -0,0 +1,15 @@
+#Data Type 
+
+print("Hello"[2])
+print(123)
+print(True)
+print(len("hello"))
+print(type("hello"))
+print(int("123"))
+
+#x=input ("my name ")
+#print("The number of letter in my name is :" + str(len(x)))
+
+print (6/3)
+print(5//2)
+print(2**3)
