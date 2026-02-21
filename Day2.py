f6f831342122fc5ac6c62323237846130cd0540e
nathnael144 <nathnael.biresaw@gmail.com>
2026-02-21 20:16:02 +0500

    Fix formatting in tip calculator prompts for clarity

--- Day2.py
+++ Day2.py
@@ -39,8 +39,8 @@ print(f"your score is = {score}, your height is {height}. you are winning: {Winn
 
 #project
 print("Welcome to the tip calculator!")
-bill=input("What was the total bill?")
-tip= input("How much tip would you like to give? 10, 12 or 15?")
+bill=input("What was the total bill? $")
+tip= input("How much tip would you like to give? 10, 12 or 15? ")
 people= input("How many people to split the bill?")
 
 total= (float(bill)*(float(tip)/100) +float(bill))/int(people)
