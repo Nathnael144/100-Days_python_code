767d9294401bf0a4f4d85b05af49b02beba96582
nathnael144 <nathnael.biresaw@gmail.com>
2026-03-06 12:52:43 +0500

    Add password generator project description and implementation details

--- Day5.py
+++ Day5.py
@@ -55,9 +55,58 @@ for number in range (1,101):
     elif number%5==0:
         print("Buzz")
     else:
-        print(number)
-        
-    
+        print(number
+              
+#project
+'''    Query successful
+
+Gemini said
+
+Based on the image provided, here is the text transcribed from the code editor and the project description panel:
+Left Side: Python Code Editor
+Python
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', ...
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# Easy Level
+
+Right Side: Project Description
+
+Password Generator Project
+Description
+... and loops to complete the challenge.
+
+Easy Version
+Generate the password in sequence. Letters, then symbols, then numbers. If the user wants 4 letters, 2 symbols and 3 numbers then the password might look like this:
+*fgdx$924
+
+You can see that all the letters are together. All the symbols are together and all the numbers follow each other as well. Try to solve this problem first.
+
+Hard Version
+When you've completed the easy version, you're ready to tackle the hard version. In the advanced version of this project the final password does not follow a pattern. So the example above might look like this:
+x$d24g*f9
+
+And every time you generate a password, the positions of the symbols, numbers, and letters are different. This will make the password more difficult...'''
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', ...
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# Easy Level        
+for i in letters:
+
+           
     
 
     
